7ee0c6dba18faef17b3ff0ca98218cb7b6a46782
tangxiangong <tangxiangong@gmail.com>
2026-04-08 17:00:45 +0800

    refactor: remove gpui GUI code, prepare workspace for Tauri

--- src/components/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod content_panel;
-pub mod header;
-pub mod history_card;
-pub mod nav_sidebar;
-pub mod settings_form;
-pub mod summary_row;
-pub mod task_card;
--- src/views/settings_page.rs
+++
@@ -1,8 +0,0 @@
-use crate::{components::settings_form::settings_form, views::YuShiGUI};
-use gpui::*;
-
-impl YuShiGUI {
-    pub fn render_settings(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> AnyElement {
-        settings_form(self, cx)
-    }
-}
